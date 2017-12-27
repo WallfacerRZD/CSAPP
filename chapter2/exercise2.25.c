7f17f885430edb44e98bb30198d8ff9a28d71455
WallfacerRZD <1048058400@qq.com>
2017-12-27 21:42:24 +0800

    'cast'

---
+++ chapter2/exercise2.25.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int i;
+    unsigned length = 0;
+    // length永远为整数导致死循环
+    for (i = 0; i <= length-1; ++i) {
+        printf("infinite loop!!\n");
+    }
+    return 0;
+}
