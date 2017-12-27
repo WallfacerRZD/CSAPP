57842e29ad1167bff9c9949e6cece02996eac1f8
WallfacerRZD <1048058400@qq.com>
2017-12-27 21:49:04 +0800

    ''

--- chapter2/exercise2.25.c
+++ chapter2/exercise2.25.c
@@ -3,7 +3,7 @@
 int main() {
     int i;
     unsigned length = 0;
-    // length永远为整数导致死循环
+    // length永远为正数导致死循环
     for (i = 0; i <= length-1; ++i) {
         printf("infinite loop!!\n");
     }
