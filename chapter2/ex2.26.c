6bbc82a1ebe07eae4e0c0881692563d178a797ce
WallfacerRZD <1048058400@qq.com>
2017-12-30 19:25:32 +0800

    ''

--- chapter2/ex2.26.c
+++ chapter2/ex2.26.c
@@ -2,7 +2,7 @@
 #include<string.h>
 
 int strlonger(const char *s, const char *t) {
-    // 当s比t长时出现bug
+    // 当s比t短时出现bug
     return strlen(s) - strlen(t) > 0;
     // return strlen(s) > strlen(t);
 }
