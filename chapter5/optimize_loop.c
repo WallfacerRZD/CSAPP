145dafd1898620ead4b3aebdf61ca1ef8e3b897e
WallfacerRZD <1048058400@qq.com>
2018-01-13 23:35:13 +0800

    optimize loop

--- chapter5/optimize_loop.c
+++ chapter5/optimize_loop.c
@@ -4,8 +4,8 @@
 
 #define OP +
 #define IDENT 0
-
 typedef long data_t; 
+
 typedef struct {
     data_t *data;
     long length;
@@ -42,6 +42,7 @@ vec_ptr get_new_vec(long len) {
 }
 
 void combine1(vec_ptr v, data_t *dest) {
+    clock_t begin = clock();
     long i;
     *dest = IDENT;
     for (i = 0; i < get_vec_len(v); ++i) {
@@ -49,9 +50,12 @@ void combine1(vec_ptr v, data_t *dest) {
         get_vec_element(v, i, &val);
         *dest = *dest OP val;
     }
+    clock_t end = clock();
+    printf("combine1:%d\n", end-begin);
 }
 
 void combine2(vec_ptr v, data_t *dest) {
+    clock_t begin = clock();
     long i;
     *dest = IDENT;
     // 避免重复计算len, 但程序的瓶颈并不在此
@@ -61,18 +65,51 @@ void combine2(vec_ptr v, data_t *dest) {
         get_vec_element(v, i, &val);
         *dest = *dest OP val;
     }
+    clock_t end = clock();
+    printf("combine2:%d\n", end-begin);
 }
 
+data_t* get_vec_start(vec_ptr v) {
+    return v->data;
+}
+
+void combine3(vec_ptr v, data_t *dest) {
+    clock_t begin = clock();
+    long i;
+    *dest = IDENT;
+    long len = get_vec_len(v);
+    // 减少过程调用
+    data_t *data = get_vec_start(v);
+    for (i = 0; i < len; ++i) {
+        *dest = *dest OP data[i];
+    }
+    clock_t end = clock();
+    printf("combine3:%d\n", end-begin);
+}
+
+void combine4(vec_ptr v, data_t *dest) {
+    clock_t begin = clock();
+    long i;
+    *dest = IDENT;
+    long len = get_vec_len(v);
+    data_t *data = get_vec_start(v);
+    // 减少内存引用
+    data_t acc = IDENT;
+    for (i = 0; i < len; ++i) {
+        acc = acc OP data[i];
+    }
+    *dest = acc;
+    clock_t end = clock();
+    printf("combine4:%d\n", end-begin);
+}
 
 int main() {
-    vec_ptr test_vec = get_new_vec(9999999);
+    vec_ptr test_vec = get_new_vec(99999999);
     data_t dest;
-
-    clock_t begin = clock();
+    combine1(test_vec, &dest);
     combine2(test_vec, &dest);
-    clock_t end = clock();
-
-    printf("%d\n", end-begin);
+    combine3(test_vec, &dest);
+    combine4(test_vec, &dest);
     free(test_vec);
     return 0;
 }
